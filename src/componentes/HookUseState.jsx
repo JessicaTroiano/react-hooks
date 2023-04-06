ae45be13167ab9109e38aef57a7a0fdb2607de27
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-04-06 15:51:22 -0300

    feat: add hook use state

---
+++ src/componentes/HookUseState.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+const HookUseState = () => {
+  let userName = "maria";
+  const [name, setName] = useState("maria");
+
+  const getName = () => {
+    userName = "José";
+    setName("Maria José");
+  }
+  //state altera o nome diretamente na tela/componente, pois estimula sua re-renderização.
+  return(
+      <>
+      <div>
+        <h2>Use State</h2>
+        <p>Variavel: {userName}</p>
+        <p>Variável: {name}</p>
+        <button onClick={getName}>Botão</button>
+      </div>
+      </>
+  )
+}
+
+export default HookUseState;
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import UseStateHook from '../componentes/HookUseState';
+
+const Home = () => {
+    return (
+      <><h1>Teste</h1><div><UseStateHook /></div></>
+    )
+}
+
+export default Home;
