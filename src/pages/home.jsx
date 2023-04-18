05fe82cda07b00bf4cdbe5bf06d945fbfae0370d
Jéssica Aline Troiano <jessycatroiano@gmail.com>
2023-04-18 10:10:45 -0300

    feat: add hook useReducer

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import HookUseReducer from "../componentes/HookUseReducer";
 import UseStateHook from '../componentes/HookUseState';
 
 const Home = () => {
     return (
-      <><h1>Teste</h1><div><UseStateHook /></div></>
+      <><h1>Teste</h1><div><UseStateHook /></div>
+      <div><HookUseReducer /></div></>
     )
 }
 
